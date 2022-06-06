943b33a35e51237deb1e3865da5f313868a4e8e9
Ershad Teymurlu <7ahgfnr@code.edu.az>
2022-06-06 06:30:37 +0400

    Blink MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Blink.DAL;
+using Blink.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace Blink.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _db { get; }
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeViewModel hvm = new HomeViewModel()
+            {
+                BlogPosts = await _db.BlogPosts.Include(x => x.Category).ToListAsync()
+            };
+            return View(hvm);
+        }
+    }
+}
---
+++ Models/BlogPost.cs
@@ -0,0 +1,12 @@
+﻿namespace Blink.Models
+{
+    public class BlogPost : BaseEntity
+    {
+        public string ImgPath { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string hint { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
